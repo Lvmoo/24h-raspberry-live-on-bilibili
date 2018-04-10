1c01addef71a5877220f4ed7c1155a0919250e54
Lvmoo <kay2u@live.shop.edu.cn>
2018-04-10 15:29:05 +0800

    Update var_set.py
    和原项目代码同步

--- var_set.py
+++ var_set.py
@@ -20,15 +20,12 @@ rtmp = 'rtmp://txy.live-send.acg.tv/live-txy/'
 #直播给的两个码，填在这里
 live_code = ''
 
-
 free_space=15360
 
 #允许download/default_mp3文件夹占用空间大小，超过时自动按时间顺序删除视频/音乐，单位：MiB
 
-
 maxbitrate='3000'
 #允许的最大码率，单位k，字符串类型，切勿改成数值型
-
 
 dm_size=20
 #每段弹幕的最大长度（20级以后可发30字）
